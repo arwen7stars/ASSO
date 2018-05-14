14e16cd15f18574cb6bdbb6f2981a584d1503c14
Claudia Marinho <claudia.m.r.marinho@gmail.com>
2018-05-14 04:40:44 +0100

    add language done

--- WorldOfPatterns/Frontend/src/app/add-language/add-language.component.ts
+++ WorldOfPatterns/Frontend/src/app/add-language/add-language.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Language} from "../language";
+import {Pattern} from "../pattern";
+import {PatternService} from "../pattern.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {LanguageService} from "../language.service";
 
 @Component({
   selector: 'app-add-language',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-language.component.css']
 })
 export class AddLanguageComponent implements OnInit {
+  public loading = true;
+  public updating = false;
 
-  constructor() { }
+  @Input() patterns: Pattern[];
+  @Input() otherPatterns: Pattern[];
+
+  constructor(
+    private patternService: PatternService,
+    private languageService: LanguageService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.patterns = [];
+    this.getPatterns();
   }
 
+  getPatterns(): void {
+    this.loading = true;
+    this.patternService.getPatterns()
+      .subscribe(patterns => {
+        this.otherPatterns = patterns;
+        this.loading = false;
+      });
+  }
+
+  add(pattern: Pattern): void {
+    this.otherPatterns = this.otherPatterns.filter(p => p !== pattern);
+    this.patterns.push(pattern);
+  }
+
+  delete(pattern: Pattern): void {
+    this.patterns = this.patterns.filter(p => p !== pattern);
+    this.otherPatterns.push(pattern);
+  }
+
+  submit(name: string): void {
+    this.updating = true;
+    var ids = [];
+    this.patterns.forEach((pattern) => { ids.push(pattern.id) });
+
+    this.languageService.addLanguage(name, ids)
+      .subscribe(() => {
+        this.router.navigate(['/languages']);
+        this.updating = false;
+      });
+  }
 }
